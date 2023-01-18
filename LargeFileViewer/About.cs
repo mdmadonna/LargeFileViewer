8523a54ebacc8c3b73306f7d0c7ce9b41c4d5927
mdmadonna <marcel_madonna@ajmsoft.com>
2023-01-17 22:06:07 -0500

    Update Links

--- LargeFileViewer/About.cs
+++ LargeFileViewer/About.cs
@@ -23,10 +23,10 @@ namespace LargeFileViewer
             lblVersion.Text = Assembly.GetEntryAssembly().GetName().Version.ToString();
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
             lblCompileDate.Text = File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location).ToString();
-            lblCopy.Text = "\u00a9 2023 AJM Software, L.L.C.";
+            lblCopy.Text = "Copyright \u00a9 2023 AJM Software, L.L.C.";
 
             HttpClient client = new HttpClient();
-            string licenseURI = "https://raw.githubusercontent.com/mdmadonna/LargeFileViewer/main/LICENSE";
+            string licenseURI = "https://raw.githubusercontent.com/mdmadonna/LargeFileViewer/master/LICENSE.txt";
             try
             {
                 string responseBody = client.GetStringAsync(licenseURI).Result;
